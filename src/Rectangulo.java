b50ca21ec107ff3bd98ca87b4c03be578dfd439b
Saul Ventura <saul22ventura@gmail.com>
2024-10-02 21:12:03 -0600

    circulo y menu

--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -5,6 +5,15 @@ public class Rectangulo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 Scanner scanner = new Scanner(System.in);
+		 char opc;
+		 System.out.println("Selecciona la Figura ");
+		 System.out.println("a) Rectangulo");
+		 System.out.println("b) Circulo");
+		 System.out.println("c) Triangulo");
+		 System.out.println("x) Salir");
+		 opc = scanner.next().charAt(0);
+		 switch (opc) {
+		 case ('a') :
 	     // Solicitar al usuario las dimensiones del rectángulo
 	        System.out.print("Ingrese la base del rectángulo: ");
 	        double base = scanner.nextDouble();
@@ -19,6 +28,18 @@ public class Rectangulo {
 	        // Mostrar los resultados
 	        System.out.println("Área del rectángulo: " + area);
 	        System.out.println("Perímetro del rectángulo: " + perimetro);
+	        break;
+		 case ('b'): 
+			 System.out.print("Ingrese el radio del circulo: ");
+		 	 float radio = scanner.nextFloat();
+			 float area2 = 3.146f*radio;
+			 area2 = area2*area2;
+			 System.out.println("Área del Circulo: " + area2); break;
+		 case ('c'): break;
+		 
+		 case ('x') : System.out.println ("Adios, vuelva pronto");
+			 
+		 } while (opc != 'x');
 	}
-
+	
 }
